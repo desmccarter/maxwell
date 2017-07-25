ebba847275eff4416f35064a85ccbd5ceee8a3a2
Des McCarter <Des.McCarter@bjss.com>
2017-07-25 13:07:32 +0100

    Bug invest - expedia

--- all/actionsamples/expedia/flights/tests/TestExpediaFlights.cs
+++ all/actionsamples/expedia/flights/tests/TestExpediaFlights.cs
@@ -21,7 +21,7 @@ namespace actionsamples.expedia.flights.tests
             using (Page page = GetOpenedPage("ExpediaPage"))
             {
                 page.Click("FlightsLink");
-                /*page.SetText("FlightsFromBox", "London, England, UK (LHR-Heathrow)");
+                page.SetText("FlightsFromBox", "London, England, UK (LHR-Heathrow)");
                 page.SetText("FlightsToBox", "Melbourne, VIC, Australia(MEL - All Airports)");
                 page.SetDropdown("AdultsDropdown", "3");
                 page.SetText("DepartingBox", "21/10/2017");
@@ -29,7 +29,7 @@ namespace actionsamples.expedia.flights.tests
                 page.SetDropdown("ChildrenDropdown", "3");
                 page.SetDropdown("ChildAge1Dropdown", "10");
                 page.SetDropdown("ChildAge2Dropdown", "12");
-                page.SetDropdown("ChildAge3Dropdown", "14");*/
+                page.SetDropdown("ChildAge3Dropdown", "14");
 
 
                 page.DoubleClick("SearchButton");
